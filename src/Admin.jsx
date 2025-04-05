bfd9cf0d41d801cf2b178d04a73f4b37f5cfc3bf
Windowee <tsoukalinho@gmail.com>
2025-04-05 20:26:11 +0300

    Update Admin.jsx

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,7 +1,7 @@
 import './Admin.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import AdminLogin from './AdminLogin'; // New authentication component
-
+import SharePage from './SharePage';
 // Import your admin page components
 import AdminOrders from './AdminOrders';
 import AdminPayments from './AdminPayments';
@@ -10,33 +10,43 @@ import AdminVerifications from './AdminVerifications';
 function AdminApp() {
     return (
         <Router>
-            <AdminLogin>
-                <header>
-                    <div className="header-navbar-container">
-                        {/* Navigation Bar */}
-                        <nav className="navbar">
-                            <ul>
-                                <li><Link to="/admin/orders">Orders</Link></li>
-                                <li><Link to="/admin/payments">Payments</Link></li>
-                                <li><Link to="/admin/verifications">Verifications</Link></li>
-                            </ul>
-                        </nav>
-                    </div>
-                </header>
+            <Routes>
+                {/* SharePage route outside of AdminLogin and header */}
+                <Route path="/event/:id" element={<SharePage />} />
 
-                {/* Main Content */}
-                <main>
-                    <Routes>
-                        <Route path="/admin/orders" element={<AdminOrders />} />
-                        <Route path="/admin/payments" element={<AdminPayments />} />
-                        <Route path="/admin/verifications" element={<AdminVerifications />} />
-                    </Routes>
-                </main>
-            </AdminLogin>
+                {/* Admin routes wrapped in AdminLogin */}
+                <Route
+                    path="/admin/*"
+                    element={
+                        <AdminLogin>
+                            <header>
+                                <div className="header-navbar-container">
+                                    {/* Navigation Bar */}
+                                    <nav className="navbar">
+                                        <ul>
+                                            <li><Link to="/admin/orders">Orders</Link></li>
+                                            <li><Link to="/admin/payments">Payments</Link></li>
+                                            <li><Link to="/admin/verifications">Verifications</Link></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </header>
+
+                            {/* Main Content */}
+                            <main>
+                                <Routes>
+                                    <Route path="orders" element={<AdminOrders />} />
+                                    <Route path="payments" element={<AdminPayments />} />
+                                    <Route path="verifications" element={<AdminVerifications />} />
+                                </Routes>
+                            </main>
+                        </AdminLogin>
+                    }
+                />
+            </Routes>
         </Router>
     );
 }
 
 export default AdminApp;
-
 
